bf43f5110fc7fa19211e7b62699346f691790b26
ben roberts <bendando@hotmail.co.uk>
2022-10-25 11:15:03 +0100

    menu working...

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
-import '../styles/globals.css'
+import '../public/assets/css/font-icons.css'
+import '../public/assets/css/plugins.css';
+import '../public/assets/css/style.css';
+import '../public/assets/css/responsive.css';
+
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return (
+		<>
+			<Script src='/assets/js/plugins.js' />
+			<Script src='/assets/js/main.js' />
+			<Component {...pageProps} />
+		</>)
 }
 
 export default MyApp
