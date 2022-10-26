bb6c4f3682322e6c70a085fdf43d7a42907f49bb
ben roberts <bendando@hotmail.co.uk>
2022-10-26 11:28:40 +0100

    added testimonials section

--- components/section-components/testimonial-v1.js
+++ components/section-components/testimonial-v1.js
@@ -1,119 +1,94 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import parse from 'html-react-parser';
+import Link from 'next/link';
 
 class Testimonial extends Component {
 
-    render() {
+	render() {
 
-        let publicUrl = process.env.PUBLIC_URL+'/'
-        let imagealt = 'image'
+		return <div className="ltn__testimonial-area section-bg-1--- bg-image-top pt-115 pb-70" data-bs-bg="/assets/img/bg/20.jpg" >
+			<div className="container">
+				<div className="row">
+					<div className="col-lg-12">
+						<div className="section-title-area ltn__section-title-2--- text-center">
+							<h6 className="section-subtitle section-subtitle-2 ltn__secondary-color">Our Testimonials</h6>
+							<h1 className="section-title">Clients Feedback</h1>
+						</div>
+					</div>
+				</div>
+				<div className="row ltn__testimonial-slider-5-active slick-arrow-1">
+					<div className="col-lg-4">
+						<div className="ltn__testimonial-item ltn__testimonial-item-7">
+							<div className="ltn__testimoni-info">
+								<p><i className="flaticon-left-quote-1" />
+									BIG Thanks to you and the team for the amazing job you did! The flat is absolutely beautiful, and I couldn't be happier :)</p>
+								<div className="ltn__testimoni-info-inner">
 
-    return <div className="ltn__testimonial-area section-bg-1--- bg-image-top pt-115 pb-70" data-bs-bg={publicUrl+"assets/img/bg/20.jpg"}>
-			  <div className="container">
-			    <div className="row">
-			      <div className="col-lg-12">
-			        <div className="section-title-area ltn__section-title-2--- text-center">
-			          <h6 className="section-subtitle section-subtitle-2 ltn__secondary-color">Our Testimonial</h6>
-			          <h1 className="section-title">Clients Feedback</h1>
-			        </div>
-			      </div>
-			    </div>
-			    <div className="row ltn__testimonial-slider-5-active slick-arrow-1">
-			      <div className="col-lg-4">
-			        <div className="ltn__testimonial-item ltn__testimonial-item-7">
-			          <div className="ltn__testimoni-info">
-			            <p><i className="flaticon-left-quote-1" /> 
-			              Precious ipsum dolor sit amet
-			              consectetur adipisicing elit, sed dos
-			              mod tempor incididunt ut labore et
-			              dolore magna aliqua. Ut enim ad min
-			              veniam, quis nostrud Precious ips
-			              um dolor sit amet, consecte</p>
-			            <div className="ltn__testimoni-info-inner">
-			              <div className="ltn__testimoni-img">
-			                <img src={publicUrl+"assets/img/testimonial/1.jpg"} alt="#" />
-			              </div>
-			              <div className="ltn__testimoni-name-designation">
-			                <h5>Jacob William</h5>
-			                <label>Selling Agents</label>
-			              </div>
-			            </div>
-			          </div>
-			        </div>
-			      </div>
-			      <div className="col-lg-4">
-			        <div className="ltn__testimonial-item ltn__testimonial-item-7">
-			          <div className="ltn__testimoni-info">
-			            <p><i className="flaticon-left-quote-1" /> 
-			              Precious ipsum dolor sit amet
-			              consectetur adipisicing elit, sed dos
-			              mod tempor incididunt ut labore et
-			              dolore magna aliqua. Ut enim ad min
-			              veniam, quis nostrud Precious ips
-			              um dolor sit amet, consecte</p>
-			            <div className="ltn__testimoni-info-inner">
-			              <div className="ltn__testimoni-img">
-			                <img src={publicUrl+"assets/img/testimonial/2.jpg"} alt="#" />
-			              </div>
-			              <div className="ltn__testimoni-name-designation">
-			                <h5>Kelian Anderson</h5>
-			                <label>Selling Agents</label>
-			              </div>
-			            </div>
-			          </div>
-			        </div>
-			      </div>
-			      <div className="col-lg-4">
-			        <div className="ltn__testimonial-item ltn__testimonial-item-7">
-			          <div className="ltn__testimoni-info">
-			            <p><i className="flaticon-left-quote-1" /> 
-			              Precious ipsum dolor sit amet
-			              consectetur adipisicing elit, sed dos
-			              mod tempor incididunt ut labore et
-			              dolore magna aliqua. Ut enim ad min
-			              veniam, quis nostrud Precious ips
-			              um dolor sit amet, consecte</p>
-			            <div className="ltn__testimoni-info-inner">
-			              <div className="ltn__testimoni-img">
-			                <img src={publicUrl+"assets/img/testimonial/3.jpg"} alt="#" />
-			              </div>
-			              <div className="ltn__testimoni-name-designation">
-			                <h5>Adam Joseph</h5>
-			                <label>Selling Agents</label>
-			              </div>
-			            </div>
-			          </div>
-			        </div>
-			      </div>
-			      <div className="col-lg-4">
-			        <div className="ltn__testimonial-item ltn__testimonial-item-7">
-			          <div className="ltn__testimoni-info">
-			            <p><i className="flaticon-left-quote-1" /> 
-			              Precious ipsum dolor sit amet
-			              consectetur adipisicing elit, sed dos
-			              mod tempor incididunt ut labore et
-			              dolore magna aliqua. Ut enim ad min
-			              veniam, quis nostrud Precious ips
-			              um dolor sit amet, consecte</p>
-			            <div className="ltn__testimoni-info-inner">
-			              <div className="ltn__testimoni-img">
-			                <img src={publicUrl+"assets/img/testimonial/4.jpg"} alt="#" />
-			              </div>
-			              <div className="ltn__testimoni-name-designation">
-			                <h5>James Carter</h5>
-			                <label>Selling Agents</label>
-			              </div>
-			            </div>
-			          </div>
-			        </div>
-			      </div>
-			      {/*  */}
-			    </div>
-			  </div>
+									<div className="ltn__testimoni-name-designation">
+										<h5>Mrs M Nielsen</h5>
+										<label>Putney</label>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div className="col-lg-4">
+						<div className="ltn__testimonial-item ltn__testimonial-item-7">
+							<div className="ltn__testimoni-info">
+								<p><i className="flaticon-left-quote-1" />
+									The workmen from Vale were polite, clean, efficient, and respectful that I was working on my computer while they worked, thanks a lot!</p>
+								<div className="ltn__testimoni-info-inner">
+									<div className="ltn__testimoni-name-designation">
+										<h5>Mr G Sherman</h5>
+										<label>Twickenham</label>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div className="col-lg-4">
+						<div className="ltn__testimonial-item ltn__testimonial-item-7">
+							<div className="ltn__testimoni-info">
+								<p><i className="flaticon-left-quote-1" />
+									New boiler is working great, thanks for replacing at such short notice, it was going to be a very cold few days!</p>
+								<div className="ltn__testimoni-info-inner">
+									<div className="ltn__testimoni-name-designation">
+										<h5>Mrs B Tinbergen</h5>
+										<label>Richmond Upon Thames</label>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					{/* <div className="col-lg-4">
+						<div className="ltn__testimonial-item ltn__testimonial-item-7">
+							<div className="ltn__testimoni-info">
+								<p><i className="flaticon-left-quote-1" />
+									Precious ipsum dolor sit amet
+									consectetur adipisicing elit, sed dos
+									mod tempor incididunt ut labore et
+									dolore magna aliqua. Ut enim ad min
+									veniam, quis nostrud Precious ips
+									um dolor sit amet, consecte</p>
+								<div className="ltn__testimoni-info-inner">
+									<div className="ltn__testimoni-img">
+										<img src={publicUrl + "assets/img/testimonial/4.jpg"} alt="#" />
+									</div>
+									<div className="ltn__testimoni-name-designation">
+										<h5>James Carter</h5>
+										<label>Selling Agents</label>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div> */}
+					{/*  */}
+				</div>
 			</div>
-
-        }
+			<div className="btn-wrapper animated flex-center-row">
+				<Link href="/testimonials"><a className="btn theme-btn-1 btn btn-effect-1">READ MORE</a></Link>
+			</div>
+		</div>
+	}
 }
 
 export default Testimonial
--- pages/index.js
+++ pages/index.js
@@ -7,15 +7,16 @@ import Aboutv1 from '../components/section-components/about-v1';
 import AboutV2 from '../components/section-components/about-v2';
 import Aboutv3 from '../components/section-components/about-v3';
 import Featuresv1 from '../components/section-components/features-v1';
-
+import Testimonial from '../components/section-components/testimonial-v1';
 
 export default function Home() {
 	return (
 		<>
 			<Navbar />
 			<Banner />
-			<Featuresv1 customClass="ltn__feature-area section-bg-1 pt-120 pb-90 mb-120---" />
 			<Aboutv3 />
+			<Featuresv1 customClass="ltn__feature-area section-bg-1 pt-120 pb-90 mb-120---" />
+			<Testimonial />
 			{/* <AboutV2 />
 			<Aboutv1 /> */}
 		</>
