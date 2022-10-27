ca95bc4cacb6bfb488669413672ad6f38ed57a14
ben roberts <bendando@hotmail.co.uk>
2022-10-27 15:56:12 +0100

    handyman & Exterior Works

--- pages/services/exterior-works.js
+++ pages/services/exterior-works.js
@@ -2,38 +2,17 @@ import React from 'react';
 import Navbar from '../../components/global-components/navbar-v2';
 import PageHeader from '../../components/global-components/page-header';
 import Footer from '../../components/global-components/footer';
-import RefurbishmentService from '../../components/services/Refurbishments';
+import ExteriorWorksService from '../../components/services/ExteriorWorks';
 
-const Refurbishments = () => {
+const ExteriorWorks = () => {
 	return (
 		<>
 			<Navbar />
-			<PageHeader headertitle="Refurbishments" subheader="Refurbishments" />
-			<RefurbishmentService />
+			<PageHeader headertitle="Exterior Works" subheader="Exterior Works" />
+			<ExteriorWorksService />
 			<Footer />
 		</>
 	);
 };
 
-export default Refurbishments;
-
-
-
-// import React from 'react';
-// import Navbar from './global-components/navbar';
-// import PageHeader from './global-components/page-header';
-// import ServiceDetails from './section-components/service-details';
-// import CallToActionV1 from './section-components/call-to-action-v1';
-// import Footer from './global-components/footer';
-
-// const Service_Details = () => {
-//     return <div>
-//         <Navbar />
-//         <PageHeader headertitle="Service Details" subheader="Property Management" />
-//         <ServiceDetails />
-//         <CallToActionV1 />
-//         <Footer />
-//     </div>
-// }
-
-// export default Service_Details
+export default ExteriorWorks;
--- pages/services/handyman.js
+++ pages/services/handyman.js
@@ -1,39 +1,14 @@
 import React from 'react';
-import Navbar from '../../components/global-components/navbar-v2';
 import PageHeader from '../../components/global-components/page-header';
-import Footer from '../../components/global-components/footer';
-import RefurbishmentService from '../../components/services/Refurbishments';
+import HandyManService from '../../components/services/Handyman';
 
 const Refurbishments = () => {
 	return (
 		<>
-			<Navbar />
-			<PageHeader headertitle="Refurbishments" subheader="Refurbishments" />
-			<RefurbishmentService />
-			<Footer />
+			<PageHeader headertitle="Handy Man" subheader="Handy Man" />
+			<HandyManService />
 		</>
 	);
 };
 
 export default Refurbishments;
-
-
-
-// import React from 'react';
-// import Navbar from './global-components/navbar';
-// import PageHeader from './global-components/page-header';
-// import ServiceDetails from './section-components/service-details';
-// import CallToActionV1 from './section-components/call-to-action-v1';
-// import Footer from './global-components/footer';
-
-// const Service_Details = () => {
-//     return <div>
-//         <Navbar />
-//         <PageHeader headertitle="Service Details" subheader="Property Management" />
-//         <ServiceDetails />
-//         <CallToActionV1 />
-//         <Footer />
-//     </div>
-// }
-
-// export default Service_Details
